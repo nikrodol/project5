38525a0c7709bf27f9aa5418641c4912bfd26fd1
nikrodol <termohi95@gmail.com>
2018-11-11 20:36:04 +0700

    1.0

--- project5.py
+++ project5.py
@@ -3,38 +3,35 @@
 """
 import random
 
-def book_cipher (crypto_key, your_textfile, encrypting_text text=[]):
-    with open(crypto_key) as f_in:
+def book_cipher (crypto_key, your_textfile, encrypting_text, text=[]):
+    with open(crypto_key) as f_key:
         try:
-            text=f_in.readlines()
+            key_text=f_key.readlines()
         except:
             print('Oh Fuck!')
-    with open(encrypting_text) as f_out:
-        text2=f_out.read()
-    with open(encrypting_text, 'a+') as f_out:
+    with open(your_textfile) as f_in:
+        your_text=f_in.read()
+    with open(encrypting_text, 'w') as f_out:
         crypto_text=''
         i=0
-        random_number=random.randint(1, len(text))
-        random_line=text[random_number-1]#Рандомная строчка
-        print(type(text[random_number-1]))
-        for k in text2: #Шифруем по буквам
-            print(len(text2), len(text))
+        random_number=random.randint(1, len(key_text))
+        random_line=key_text[random_number-1]#Рандомная строчка
+        print(type(key_text[random_number-1]))
+        for k in your_text: #Шифруем по буквам
+            print(len(your_text), len(key_text))
             while random_line.find(text2[i]) == -1:
-                random_number=random.randint(1, len(text))
-                random_line=text[random_number-1]
+                random_number=random.randint(1, len(key_text))
+                random_line=key_text[random_number-1]
             print(str(random_number) + ':' + str(random_line.find(text2[i])))
             crypto_text+=str(random_number) + ':' + str(random_line.find(text2[i])+1) + ' '
             i+=1
             print(crypto_text)
-            random_number = random.randint(1, len(text))
-            random_line = text[random_number - 1]
-        f_out.write('')
+            random_number = random.randint(1, len(key_text))
+            random_line = key_text[random_number - 1]
+        f_out.write(crypto_text)
 
 
-
-
-chosen_file01 = input('Enter name of file for crypto-key: ') #Only "input.txt" use in repository
-chosen_file02 = input('Enter name of file for encrypting text: ')#Only "output.txt" use in repository
-chosen_file03 = input('Enter name of file for encrypting text: ')
-book_cipher(chosen_file01, chosen_file02)
-
+chosen_file01 = input('Enter name of file for crypto-key: ') #Only "key.txt" use in repository
+chosen_file02 = input('Enter name of file with yout text: ')#Only "intput.txt" use in repository
+chosen_file03 = input('Enter name of file for encrypting text: ')#Only "output.txt" use in repository
+book_cipher(chosen_file01, chosen_file02, chosen_file03)
